19363f2c58f979d462609b10b0a60c36e4cafd9b
Carlos Robles <contact@croblesm.com>
2024-01-20 00:40:49 +0000

    Precon ready!

--- azure/dab/dab-commands.sh
+++ azure/dab/dab-commands.sh
@@ -4,6 +4,8 @@ dab init --database-type mssql  --config "dab-config.json" --host-mode Developme
 # Modify dab-config.json with connection string
 dab add Speaker --config dab-config.json --source dbo.Speakers --permissions "anonymous:*"
 dab add Sponsor --config dab-config.json --source dbo.Sponsors --permissions "anonymous:*"
+dab add Session --config dab-config.json --source dbo.Sessions --permissions "anonymous:*"
+dab update Session --config dab-config.json --relationship SpeakerSession --target.entity Speaker --cardinality many  --relationship.fields "speaker_id:speaker_id"
 
 # Start DAB's engine
 dab start --config dab-config.json
